942bb72544fe3b632eda372e7c9f8578fbb8e469
ncbtart@gmail.com <ncbtart@gmail.com>
2019-07-17 13:11:12 +0200

    OK

---
+++ src/modules/article/article.utils.ts
@@ -0,0 +1,19 @@
+import { createWriteStream } from 'fs';
+import { shortid } from 'shortid';
+
+function storeUpload({ stream, filename }): Promise<any> {
+  const path = `images/${shortid.generate()}`;
+
+  return new Promise((resolve, reject) =>
+    stream
+      .pipe(createWriteStream(path))
+      .on('finish', () => resolve({ path }))
+      .on('error', reject),
+  );
+}
+
+const processUpload = async upload => {
+  const { stream, filename, mimetype, encoding } = await upload;
+  const { path } = await storeUpload({ stream, filename });
+  return path;
+};
