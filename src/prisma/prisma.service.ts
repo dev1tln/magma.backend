d1b5a79d9b04a8fc545cc91f92409eb0c5df5f0c
ncbtart@gmail.com <ncbtart@gmail.com>
2019-07-28 10:12:45 +0200

    Erreur fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,14 +3,7 @@ import { Prisma } from './prisma.binding';
 
 @Injectable()
 export class PrismaService extends Prisma {
-
   constructor() {
-    const endpoint = process.env.PRISMA_ENDPOINT;
-    console.log(`Your endpoint is ${endpoint}`); // undefined
-    // il faut ajouter dotenv le 22/07/2019
-    const dotenv = require('dotenv');
-    dotenv.config();
-    console.log(`Your endpoint is ${process.env.PRISMA_ENDPOINT}`); // affiche endpoint
     super({
       endpoint: process.env.PRISMA_ENDPOINT,
       debug: false,
